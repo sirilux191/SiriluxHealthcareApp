6c2ce52a5207315b34571d11a0bfc4c65201180f
Aaditya Chandankar <aadityachandankar@gmail.com>
2023-09-29 13:02:19 +0530

    Initial commit.

---
+++ src/mvp_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { mvp_backend } from "../../declarations/mvp_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await mvp_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
